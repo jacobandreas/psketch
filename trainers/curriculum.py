bfdf5da607118b13bee396f494678efd06d358d5
Jacob <jdandreas@gmail.com>
2017-02-17 17:38:29 -0800

    unorder

--- trainers/curriculum.py
+++ trainers/curriculum.py
@@ -147,10 +147,11 @@ class CurriculumTrainer(object):
     def train(self, model, world):
         model.prepare(world, self)
         #model.load()
-        if self.config.trainer.use_curriculum:
-            max_steps = 1
-        else:
-            max_steps = 100
+        #if self.config.trainer.use_curriculum:
+        #    max_steps = 1
+        #else:
+        #    max_steps = 100
+        max_steps = 100
         i_iter = 0
 
         task_probs = []
