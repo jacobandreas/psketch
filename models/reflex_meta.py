4690be4c66c4121e773489482e248610059caff2
Jacob <jdandreas@gmail.com>
2017-02-22 10:05:00 -0800

    .

--- models/reflex_meta.py
+++ models/reflex_meta.py
@@ -11,7 +11,7 @@ DISCOUNT = 0.9
 BATCH_SIZE = 2000
 HISTORY_SIZE = 50000
 
-CHOICES = [2, 3, 4, 5, 6, 7, 9, 10, 0]
+CHOICES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0]
 SOLUTION = [4, 5, 2, 6, 0]
 
 class ReflexMetaModel(object):
