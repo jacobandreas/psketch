30f154994f2a440ca882fb07efba6674c24d5e1e
Jacob <jdandreas@gmail.com>
2017-08-16 23:09:13 -0700

    seed

--- models/modular_ac_interactive.py
+++ models/modular_ac_interactive.py
@@ -38,7 +38,7 @@ class ModularACInteractiveModel(object):
     def __init__(self, config):
         self.experiences = []
         self.world = None
-        tf.set_random_seed(0)
+        tf.set_random_seed(config.model.seed)
         self.next_actor_seed = 0
         self.config = config
 
